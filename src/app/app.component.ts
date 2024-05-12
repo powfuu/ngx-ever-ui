41d5c605b812edd98a7d4502de85e4b91077dd01
powfuu <everitjhon@gmail.com>
2024-05-12 21:35:10 +0200

    Fix responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   stateSideSheet: boolean = true;
+
+  ngOnInit(): void {
+    this.initVercelAnalyticsAndSpeedInsights();
+  }
 
   handleSideSheet(): void {
     this.stateSideSheet = !this.stateSideSheet;
+  }
+
+  initVercelAnalyticsAndSpeedInsights(): void {
+    //Init Analytics
+    inject();
+    //Init Speed Insights
+    injectSpeedInsights();
   }
 }
